6fe1061f47c78a739c6add8b4ab5182a3137daee
Nelly <no222hh@student.lnu.se>
2024-06-13 15:37:10 +0200

    boot file

--- boot.py
+++ boot.py
@@ -1,6 +1,8 @@
-import secrets
+# boot.py -- run on boot-up
+from mysecrets import secrets
 from time import sleep
 import network
+import ntptime
 
 
 def connect():
@@ -10,7 +12,7 @@ def connect():
         wlan.active(True)                       # Activate network interface
         # set power mode to get WiFi power-saving off (if needed)
         wlan.config(pm = 0xa11140)
-        wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)  # Your WiFi Credential
+        wlan.connect(secrets["WIFI_SSID"], secrets["WIFI_PASSWORD"])  # Your WiFi Credential
         print('Waiting for connection...', end='')
         # Check if it is connected otherwise wait
         while not wlan.isconnected() and wlan.status() >= 0:
@@ -19,6 +21,13 @@ def connect():
     # Print the IP assigned by router
     ip = wlan.ifconfig()[0]
     print('\nConnected on {}'.format(ip))
+
+     # Set the time using ntp
+    try:
+        ntptime.settime()
+        print('Time synchronized successfully')
+    except Exception as e:
+        print('Failed to synchronize time:', e)
     return ip
 
 def disconnect():
