63cc7b37f60a5425cd726dee07c1674b5c99a828
Nelly <no222hh@student.lnu.se>
2024-06-20 20:37:30 +0200

    fixing in main.py

--- lib/SendEmail.py
+++ lib/SendEmail.py
@@ -1,65 +1,109 @@
 import umail
 from mysecrets import secrets
+import time
 
 # Email details
 sender_email = secrets['SENDER_EMAIL']
 sender_app_password = secrets['GOOGLE_APP_PASS']
-email_subject = '🌞Uppdatering från växthus🌞'
+email_subject = '🌞Uppdatering från fönstret🌞'
 
 def email(dayvalues, nightvalues, eveningvalues):
     daytemp = dayvalues["temp"]
     dayhumid = dayvalues["humidity"]
-    #daygroundmoist = dayvalues["groundmoist"]
-    #daylight = dayvalues["light"]
+    daygroundmoist = dayvalues["groundmoist"]
+    daylight = dayvalues["light"]
+    daytime = dayvalues["time"]
 
     eveningtemp = eveningvalues["temp"]
     eveninghumid = eveningvalues["humidity"]
-    #eveninggroundmoist = eveningvalues["groundmoist"]
-    #eveninglight = eveningvalues["light"]
+    eveninggroundmoist = eveningvalues["groundmoist"]
+    eveninglight = eveningvalues["light"]
+    eveningtime = eveningvalues["time"]
 
     nighttemp = nightvalues["temp"]
     nighthumid = nightvalues["humidity"]
-    #nightgroundmoist = nightvalues["groundmoist"]
-    #nightlight = nightvalues["light"]
+    nightgroundmoist = nightvalues["groundmoist"]
+    nightlight = nightvalues["light"]
+    nighttime = nightvalues["time"]
+
 
     email_body = f"""
       <html>
     <head></head>
     <body>
-        <h2>Dagvärden:</h2>
-        <p>Temperatur: {daytemp}</p>
-        <p>Fuktighet: {dayhumid}</p>
-        <p>Jordfuktighet: {daygroundmoist}</p>
-        <p>Ljusnivå: {daylight}</p>
+        <h2>Dagvärden ({daytime}):</h2>
+        <p>Temperatur: {daytemp}°C</p>
+        <p>Fuktighet: {dayhumid}%</p>
+        <p>Jordfuktighet: {daygroundmoist}%</p>
+        <p>Ljusnivå: {daylight}lm</p>
         
-        <h2>Kvällsvärden:</h2>
-        <p>Temperatur: {eveningtemp}</p>
-        <p>Fuktighet: {eveninghumid}</p>
-        <p>Jordfuktighet: {eveninggroundmoist}</p>
-        <p>Ljusnivå: {eveninglight}</p>
+        <h2>Kvällsvärden ({eveningtime}):</h2>
+        <p>Temperatur: {eveningtemp}°C</p>
+        <p>Fuktighet: {eveninghumid}%</p>
+        <p>Jordfuktighet: {eveninggroundmoist}%</p>
+        <p>Ljusnivå: {eveninglight}lm</p>
         
-        <h2>Nattvärden:</h2>
-        <p>Temperatur: {nighttemp}</p>
-        <p>Fuktighet: {nighthumid}</p>
-        <p>Jordfuktighet: {nightgroundmoist}</p>
-        <p>Ljusnivå: {nightlight}</p>
+        <h2>Nattvärden ({nighttime}):</h2>
+        <p>Temperatur: {nighttemp}°C</p>
+        <p>Fuktighet: {nighthumid}%</p>
+        <p>Jordfuktighet: {nightgroundmoist}%</p>
+        <p>Ljusnivå: {nightlight}lm</p>
     </body>
     </html>
     """
     return email_body
 
 
-def email_movment():
+def test_email(dayvalues):
+   daytemp = dayvalues["temp"]
+   dayhumid = dayvalues.get("humidity", "N/A")
+   daygroundmoist = dayvalues["groundmoist"]
+   daylight = dayvalues.get("light", "N/A")
+   email_body = f"""
+      <html>
+    <head></head>
+    <body>
+       <h2>Dagvärden:</h2>
+        <p>Temperatur: {daytemp}°C</p>
+        <p>Fuktighet: {dayhumid}%</p>
+        <p>Jordfuktighet: {daygroundmoist}%</p>
+        <p>Ljusnivå: {daylight}%</p>
+    </body>
+    </html>
+    """
+   return email_body
+   
 
+def test_send_email(subject, dayvalues):
+   smtp = umail.SMTP('smtp.gmail.com', 465, ssl=True)
+   smtp.login(sender_email, sender_app_password)
+   smtp.to(subject)
+   smtp.write(f"Subject: {email_subject}\n")
+   smtp.write("Content-Type: text/html\n")
+   smtp.write("\n") # Empty line to separate headers from content
+   smtp.write(test_email(dayvalues)) # Skriv e-postens ämne och innehåll
+   smtp.send()  # Skicka e-posten
+   smtp.quit()  # Stäng SMTP-sessionen
+   
+
+#ändra 2 timmar framåt
+def get_current_time():
+   current_time = time.localtime()
+   hour = (current_time[3] + 2) % 24  # Adjust for timezone
+   return f"{hour:02}:{current_time[4]:02}"
+   
+
+def email_movment():
+    current_time = get_current_time()
     email_body = f"""
     <html>
     <head></head>
     <body>
-        <h2>Rörelse Upptäckt i Växthus</h2>
-        <p>Rörelse har upptäckts i växthuset vid följande tidpunkt:</p>
-        <p><strong>Datum och Tid:</strong> </p>
-        <p><strong>Plats:</strong> Växthus</p>
-        <p>Detta är ett automatiskt meddelande skickat av växthusövervakningssystemet.</p>
+        <h2>Rörelse Upptäckt i fönstret 🐈</h2>
+        <p>Rörelse har upptäckts i fönstret vid följande tidpunkt:</p>
+        <p><strong>Tid:</strong> {current_time} </p>
+        <p><strong>Plats:</strong> Fönstret</p>
+        <p>Detta är ett automatiskt meddelande skickat av övervakningssystemet.</p>
     </body>
     </html>
     """
--- main.py
+++ main.py
@@ -10,32 +10,48 @@ import sys
 import boot
 import machine
 import ubinascii
-from lib.SendEmail import send_email_movment
+from lib.SendEmail import send_email_movment, send_email, test_send_email
 from lib.seesaw import Seesaw
 from lib.stemma_soil_sensor import StemmaSoilSensor
 
-# Defining sensors
-i2c = machine.I2C(0, sda=machine.Pin(4), scl=machine.Pin(5), freq=400000)
+# Constants
+MIN_MOISTURE_VALUE = 345 
+MAX_MOISTURE_VALUE = 1500  # Sensorns värde i vatten (100% fuktighet)
 
-# Light sensor
-led_pin = ADC(Pin(27))
+#bright_value = 0
+#dark_value = 7000
+SLEEP_INTERVAL = 600  # Sleep interval in seconds
 
-# DHT air temp and moisture
-temp_sensor = dht.DHT11(Pin(17))
+# Sensor Classes
+class LightSensor:
+    def __init__(self, pin):
+        self.sensor = ADC(Pin(pin))
+    
+    def read_value(self):
+        return self.sensor.read_u16()
 
-# Client ID, unique to the microcontroller
-CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+class TempHumiditySensor:
+    def __init__(self, pin):
+        self.sensor = dht.DHT11(Pin(pin))
+    
+    def measure(self):
+        self.sensor.measure()
+    
+    def temperature(self):
+        return self.sensor.temperature()
+    
+    def humidity(self):
+        return self.sensor.humidity()
 
-# LED Lamp 
-led = Pin("LED", Pin.OUT)
+class MovementSensor:
+    def __init__(self, pin):
+        self.sensor = Pin(pin, Pin.IN, Pin.PULL_UP)
+    
+    def is_detected(self):
+        return self.sensor.value() == 0
 
-# Movement sensor
-movment = Pin(26, Pin.IN, Pin.PULL_UP)
 
-# Ground Moisture sensor
-moiistureSensor = StemmaSoilSensor(i2c)
-
-# Build jason format for MQTT 
+# MQTT Functions
 def build_json(variable_1, value_1):
     try:
         data = {variable_1: value_1}
@@ -43,7 +59,7 @@ def build_json(variable_1, value_1):
         return retValue
     except:
         return None
-
+    
 # Sensing message to MQTT server
 def send_topic(topicObject, topicName):
     print(topicObject)
@@ -54,6 +70,32 @@ def send_topic(topicObject, topicName):
         print("FAILED")
         # We must add error hadling here if WiFi being unavailable here
 
+def connect_mqtt():
+    client = MQTTClient(
+        client_id=ubinascii.hexlify(machine.unique_id()),
+        server=secrets["MQTT_SERVER"],
+        port=secrets["MQTT_PORT"],
+        user=secrets["MQTT_USER"],
+        password=secrets["MQTT_KEY"]
+    )
+    time.sleep(0.1)
+    client.connect()
+    return client
+
+# Initialization
+# Defining sensors
+i2c = machine.I2C(0, sda=machine.Pin(4), scl=machine.Pin(5), freq=400000)
+# Light sensor
+light_sensor = LightSensor(27)
+# DHT temp and humidity
+temp_humidity_sensor = TempHumiditySensor(17)
+# Movement sensor
+movement_sensor = MovementSensor(26)
+# LED Lamp 
+led = Pin("LED", Pin.OUT)
+# Soil sensor
+moisture_sensor = StemmaSoilSensor(i2c)
+
 # WiFi Connection
 try:
     ip = boot.connect()
@@ -62,9 +104,7 @@ except KeyboardInterrupt:
 
 # Connect to MQTT server
 try:
-    client = MQTTClient(client_id=CLIENT_ID, server=secrets["MQTT_SERVER"], port=secrets["MQTT_PORT"], user=secrets["MQTT_USER"], password=secrets["MQTT_KEY"])
-    time.sleep(0.1)
-    client.connect()
+    client = connect_mqtt()
     print(f"Connected to MQTT server at {secrets["MQTT_SERVER"]}")
 except Exception as error:
     sys.print_exception(error, sys.stderr)
@@ -73,52 +113,114 @@ except Exception as error:
     print("Disconnected from WiFi.")
     sys.exit()
 
-# Main loop for reading sensor data
+def read_sensors():
+    try:
+        temp_humidity_sensor.measure()
+        temperature = temp_humidity_sensor.temperature()
+        humidity = temp_humidity_sensor.humidity()
+        light_value = light_sensor.read_value()
+        darkness = round(light_value / 65535 * 100, 2)
+        raw_moisture = moisture_sensor.get_moisture()
+        moisture_percentage = round((raw_moisture - MIN_MOISTURE_VALUE) / (MAX_MOISTURE_VALUE - MIN_MOISTURE_VALUE) * 100, 2)
+
+        print(f"Read Sensors - Temperature: {temperature}, Humidity: {humidity}, Light: {darkness}, Moisture: {moisture_percentage}%")
+
+        return {
+            "temperature": temperature,
+            "humidity": humidity,
+            "light": darkness,
+            "moisture": moisture_percentage
+        }
+    except Exception as e:
+        print(f"Error reading sensors: {e}")
+        return None
+
+
+
+def send_sensor_data(sensor_data):
+    temp_json = build_json("temperature", sensor_data["temperature"])
+    humidity_json = build_json("humidity", sensor_data["humidity"])
+    light_json = build_json("light", sensor_data["light"])
+    soil_json = build_json("soil", sensor_data["moisture"])
+    
+    if temp_json:
+        send_topic(temp_json, secrets["MQTT_TEMPERATURE_FEED"])
+    if humidity_json:
+        send_topic(humidity_json, secrets["MQTT_HUMIDITY_FEED"])
+    if light_json:
+        send_topic(light_json, secrets["MQTT_LIGHT_FEED"])
+    if soil_json:
+        send_topic(soil_json, secrets["MQTT_SOIL_FEED"])
+
+#temp_sensor.measure()
+#tempValue = temp_sensor.temperature()
+#humidValue = temp_sensor.humidity()
+
+#raw_moisture = moistureSensor.get_moisture()
+
+# Omvandla till procentandel (kalibrerad)
+#moisture_percentage = round((raw_moisture - min_moisture_value) / (max_moisture_value - min_moisture_value) * 100, 2)
+#light_value = led_pin.read_u16()
+
+# Beräkna ljus i procent baserat på maximalt ljusvärde
+#light_percentage = int((light_value / dark_value) * 100)
+
 try:
+    previous_day = None
+    day_values = night_values = evening_values = {}
     while True:
         try:
-            movment_detected = movment.value()
-            print(movment_detected)
-            light_value = led_pin.read_u16()
-            print(f"Light sensor value: {light_value}")
-            mooisture = moiistureSensor.get_moisture()
-            print(f"Moisture: {mooisture}")
-            darkness = round(light_value / 65535 * 100, 2)
-            print(f"Darkness: {darkness}%")
-            if darkness >= 70:
-                print("Darkness is {darkness}%, LED turned on".format(darkness))
-                led.on()
-            else:
-                print("It is enough light, no need to turn the LED on")
-                led.off()
-            if movment_detected == 0:  
-                print("Movement detected")
-                send_email_movment("gamingbullarna@gmail.com")
-                print("Mail was sent!")
-            temp_sensor.measure()
-            temperature = temp_sensor.temperature()
-            humidity = temp_sensor.humidity()
-            print(f"Temperature: {temperature}°C, Humidity: {humidity}%")
+            sensor_data = read_sensors()
+            if sensor_data is None:
+                print("Sensor data is None, skipping this loop iteration.")
+                continue
             
-            tempObj = build_json("temperature", temperature)
-            humidityObj = build_json("humidity", humidity)
-            lightObj = build_json("light", light_value)
-            soilObj = build_json("soil", mooisture)
+            print(f"Sensor Data: {sensor_data}")
             
+            current_time = time.localtime()
+            hour = (current_time[3] + 2) % 24  # Adjust for timezone
+            day = current_time[2]
+            current_time_str = f"{hour:02}:{current_time[4]:02}"
             
-            if tempObj:
-                send_topic(tempObj, secrets["MQTT_TEMPERATURE_FEED"])
-            if humidityObj:
-                send_topic(humidityObj, secrets["MQTT_HUMIDITY_FEED"])
-            if lightObj:
-                send_topic(lightObj, secrets["MQTT_LIGHT_FEED"])
-            if soilObj:
-                send_topic(soilObj, secrets["MQTT_SOIL_FEED"])
-            
-            time.sleep(15)  # Wait for 15 seconds before next reading
+            values = {
+                "temp": sensor_data["temperature"],
+                "humidity": sensor_data["humidity"],
+                "groundmoist": sensor_data["moisture"],
+                "light": sensor_data["light"],
+                "time": current_time_str
+            }
+
+
+            #if movement_sensor.is_detected():
+                #try:
+                    #print("Movement detected")
+                   # send_email_movment("nossfolonelly@gamil.com")
+                    #send_email_movment("simonberg92@hotmail.se")
+                    #print("Email sent successfully!")
+                #except Exception as e:
+                    #print(f"Email sending error: {e}")
+
+            if hour == 3 and current_time[4] == 0 and day != previous_day:
+                night_values = values.copy()
+            if hour == 13 and current_time[4] == 0 and day != previous_day:
+                day_values = values.copy()
+            if hour == 18 and current_time[4] == 0 and day != previous_day:
+                evening_values = values.copy()
+                try:
+                    send_email("nossfolonelly@gmail.com", day_values, night_values, evening_values)
+                    previous_day = day
+                    print("Email sent successfully")
+                except Exception as e:
+                    print(f"Email sending error: {e}")
+
+            send_sensor_data(sensor_data)
+            time.sleep(SLEEP_INTERVAL)
+        except KeyboardInterrupt:
+            print("Program interrupted by user")
+            break
         except Exception as e:
-          print(f"Error in main loop: {e}")
-          time.sleep(5)  # Wait a bit before retrying
+            print(f"Error in main loop: {e}")
+            time.sleep(5)
 except KeyboardInterrupt:
     print("Interrupted by user")
 finally:
